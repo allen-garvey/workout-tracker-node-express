30a68f0e8f6ed204ab9b2b8981a13ade0a69d688
Allen Garvey <allengarvey@hotmail.com>
2016-05-30 23:14:12 -0400

    add stud for workout save route

--- app.js
+++ app.js
@@ -24,12 +24,28 @@ app.get('/',function(req,res){
 	res.sendFile(config.viewsDir + 'index.html');
 });
 
+//get all the workouts
 app.get('/api/workouts',function(req,res){
 	var workouts = {};
 	workouts.data = [{id: 1, name: 'benchpress', date: '2015-10-01', lbs:true, weight: 100, reps:10}, {id: 2, name: 'squat', date: '2016-11-11', lbs:false, weight: 300, reps:2}];
 	res.json(workouts);
 });
 
+var workoutId = 10;
+//create new workout
+app.post('/api/workouts',function(req,res){
+	console.log(req.body);
+	var workout = {id: workoutId};
+	workout.name = req.body.name;
+	workout.date = req.body.date;
+	workout.reps = req.body.reps;
+	workout.lbs = req.body.lbs;
+	workout.weight = req.body.weight;
+	workoutId++;
+	res.json(workout);
+});
+
+//delete a workout
 app.delete('/api/workouts/:id',function(req,res){
 	res.json({success: true, id : req.params.id});
 });
